4ad24a15bc874157b0ca537ae507dbbfca35e825
Tyler <delrosariotyler@gmail.com>
2023-07-12 18:49:31 -0500

    Added footer stuff

--- test.js
+++ test.js
@@ -3,3 +3,34 @@ function helloWorld (int) {
 }
 
 console.log(helloWorld(22));
+
+function nthPower(arr, n) {
+
+    //Check whether num is a power of N.
+    function recursionN(num, index) {
+        //If N to the power of index == num return true.
+        if (n**index == num) {
+            return true;
+        }
+
+        //If N to the power of index doesn't equal index and is less than
+        //num, then run recursionN again and increase index by one.
+        else if (n**index < num) {
+            return recursionN(num, index+1);
+        }
+
+        //N to the power of index is greater than num and therefore num
+        //is not a power of N.
+        return false;
+
+    }
+
+    return arr.filter((element) => {
+        
+        if (recursionN(element)) {
+            return element;
+        }
+
+    });
+
+}
